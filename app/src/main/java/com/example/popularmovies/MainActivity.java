d1a523a13b472550571dbedf184f2812cfc6e5bb
Alan Zheng <aszheng12@gmail.com>
2020-04-29 00:39:43 -0700

    Implemented RecyclerView and onClick

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.popularmovies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class MainActivity extends AppCompatActivity implements MovieAdapter.MovieAdapterOnClickHandler {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private RecyclerView mRecyclerView;
+    private MovieAdapter mMovieAdapter;
+
+    private TextView mErrorMessageDisplay;
+
+    String[] movieFakeData = new String[] { "Movie A", "Movie B", "Movie C", "Movie D", "Movie E" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_movies);
+
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+        mRecyclerView.setLayoutManager(layoutManager);
+        mRecyclerView.setHasFixedSize(true);
+
+        mMovieAdapter = new MovieAdapter(this);
+        mRecyclerView.setAdapter(mMovieAdapter);
+
+        showMovieDataView();
+    }
+
+    public void showMovieDataView () {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility((View.VISIBLE));
+
+        mMovieAdapter.setMovieData(movieFakeData);
+    }
+
+    public void showErrorMessage () {
+        mRecyclerView.setVisibility((View.INVISIBLE));
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onClick(String movieSelected) {
+        Context context = this;
+        Log.d(TAG, movieSelected + " clicked");
     }
 }
---
+++ app/src/main/java/com/example/popularmovies/MovieAdapter.java
@@ -0,0 +1,73 @@
+package com.example.popularmovies;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MovieAdapter extends RecyclerView.Adapter <MovieAdapter.MovieAdapterViewHolder> {
+
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+
+    private String [] mMovieData;
+
+    private final MovieAdapterOnClickHandler mClickHandler;
+
+    public interface MovieAdapterOnClickHandler {
+        void onClick(String movieSelected);
+    }
+
+    public MovieAdapter(MovieAdapterOnClickHandler clickHandler) {
+        mClickHandler = clickHandler;
+    }
+
+    public class MovieAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public final TextView mMovieTextView;
+
+        public MovieAdapterViewHolder (View view) {
+            super(view);
+            mMovieTextView = (TextView) view.findViewById(R.id.tv_movie_data);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+          int adapterPosition = getAdapterPosition();
+          String movieSelected = mMovieData[adapterPosition];
+          Log.d(TAG, movieSelected + " clicked");
+        }
+    }
+
+    @Override
+    public MovieAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.movie_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+        return new MovieAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieAdapterViewHolder movieAdapterViewHolder, int position) {
+        String movieSelected = mMovieData[position];
+        movieAdapterViewHolder.mMovieTextView.setText(movieSelected);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == mMovieData) return 0;
+        return mMovieData.length;
+    }
+
+    public void setMovieData(String[] movieData) {
+        mMovieData = movieData;
+        notifyDataSetChanged();
+    }
+
+}
